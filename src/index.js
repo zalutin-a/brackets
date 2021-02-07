6805091eee3a781db3ac7fdd7bd5c98935a0946e
zalutin-a <an9rey89@yandex.ru>
2021-02-08 00:11:05 +0300

    make func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+const type = {
+    ')':'(',
+    ']':'[',
+    '}':'{',
+    '|':'|',
+    '2':'1',
+    '4':'3',
+    '6':'5',
+    '7':'7',
+    '8':'8'
+}
+
+const myReg = /[\|78]/g
+
+module.exports = function check(str, bracketsConfig){
+    let arr = []
+    let result = true
+    for(let i =0; i<str.length; i++){
+        if(type.hasOwnProperty(str.charAt(i))){
+            if(!myReg.test(str.charAt(i))){
+                if(arr[arr.length-1] === type[str.charAt(i)]) arr.pop()
+                else {return result = false}
+            }else{  
+                myReg.test()
+                if(arr[arr.length-1] === str.charAt(i)) arr.pop()
+                else arr.push(str.charAt(i))
+                }
+        }else{
+                arr.push(str.charAt(i))
+                }
+        }
+    
+    if(arr.length != 0) result = false
+     
+    return result
 }
